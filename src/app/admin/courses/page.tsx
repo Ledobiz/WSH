8f6adf351b6983bac19e22ff920829f21bd0af90
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-12-24 02:57:37 +0100

    fix: Minor change

--- src/app/admin/courses/page.tsx
+++ src/app/admin/courses/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 import CoursesPage from "@/src/components/admin/pages/CoursesPage";
+import PageLoader from "@/src/components/website/PageLoader";
 
 export const metadata: Metadata = {
     title: "All Courses - Women Skills Hub",
@@ -8,7 +10,9 @@ export const metadata: Metadata = {
 
 const Courses = () => {
     return (
-        <CoursesPage />
+        <Suspense fallback={<PageLoader />}>
+            <CoursesPage />
+        </Suspense>
     )
 }
 export default Courses
