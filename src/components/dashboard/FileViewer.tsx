ce48e6cabd4bd9d77200b9f46402a973c6f1a59f
Kazeem Asifat <asifatkazeem@yahoo.com>
2026-02-07 15:45:43 +0100

    fix: Changed the PDF viewer

--- src/components/dashboard/FileViewer.tsx
+++ src/components/dashboard/FileViewer.tsx
@@ -1,39 +1,14 @@
-// import { useEffect, useRef } from "react"
+import { RPProvider, RPDefaultLayout, RPPages, RPConfig } from '@pdf-viewer/react'
 
 const FileViewer = ({fileUrl}: {fileUrl: string}) => {
-    console.log("fileUrl", fileUrl);
-    /*const viewer = useRef<any>(null);
-
-    useEffect(() => {
-        import('@pdftron/webviewer').then((module) => {
-            const WebViewer = module.default;
-
-            WebViewer(
-                {
-                    path: '/lib/webviewer',
-                    initialDoc: fileUrl,
-                },
-                viewer.current,
-            ).then((instance) => {
-                const { documentViewer } = instance.Core;
-                instance.UI.disableElements(['printButton', 'downloadButton', 'saveAsButton']);
-            });
-        });
-    }, [fileUrl]);*/
-
-
     return (
-        /*<div className="video-box webviewer" 
-            ref={viewer}
-            style={{width: "100%", height: "600px"}}
-        />*/
-
-        <div className="w-full h-[600px]">
-            <iframe 
-                src={fileUrl}
-                className="w-full h-full"
-            />
-        </div>
+        <RPConfig>
+            <RPProvider src={fileUrl}>
+                <RPDefaultLayout style={{ height: '600px' }}>
+                <RPPages />
+                </RPDefaultLayout>
+            </RPProvider>
+        </RPConfig>
     )
 }
 export default FileViewer
